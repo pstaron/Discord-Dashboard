c9bb929104be90a10d0daf09b93040432a08da3f
zakaz <piotr.staron@flask.one>
2023-10-14 23:53:46 +0200

    Test#3

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload
-          Hello From Polandv2.
+          Hello From Polandv3.
         </p>
         <a
           className="App-link"
