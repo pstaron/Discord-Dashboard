0a7fe4e7e2a05f99cc6912d2ddb436e6a75fd209
MrZakaz <adam.staron@vp.pl>
2023-10-15 00:10:20 +0200

    Test#8

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload
-          Hello From Polandv7.
+          Hello From Polandv8.
         </p>
         <a
           className="App-link"
